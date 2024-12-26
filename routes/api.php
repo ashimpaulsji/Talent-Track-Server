0ea30fb93a9a1fe018ed288337385d3a868819b9
ashimpaulsji <ashim.paul@sjinnovation.com>
2024-12-26 10:43:08 +0600

    Update changes

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
-
     require base_path('app/Modules/Auth/routes.php');
     require base_path('app/Modules/User/routes.php');
     require base_path('app/Modules/Employer/routes.php');
